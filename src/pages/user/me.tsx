60191a8e94d788a2a5a431bc355ccab5a5a393d6
Not TCA <realtcatech@gmail.com>
2023-03-29 14:08:53 +0800

    add /user/me redirect page

---
+++ src/pages/user/me.tsx
@@ -0,0 +1,29 @@
+import { RedirectToUserProfile, useUser } from "@clerk/nextjs";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Redirect } from "~/components/redirect";
+
+const RedirectPage: NextPage = () => {
+  const router = useRouter();
+  const { isLoaded: userLoaded, user } = useUser();
+
+  useEffect(() => {
+    if (!userLoaded) return;
+    void router.push(`/user/@${user?.username as string}`);
+  }, [router, userLoaded, user]);
+
+  if (!userLoaded)
+    return (
+      <Redirect>
+        <p></p>
+      </Redirect>
+    );
+
+  return (
+    <Redirect>
+      <p></p>
+    </Redirect>
+  );
+};
+export default RedirectPage;
